02c83bf2eaa6529817d23ad3e199f0d61f50dc31
Richard Cook <rcook@rcook.org>
2023-12-30 10:13:13 -0800

    Add configuration

--- render.ps1
+++ render.ps1
@@ -9,14 +9,15 @@ Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
 
 $chordProPath = Resolve-Path -Path (Join-Path -Path $env:ProgramFiles -ChildPath ChordPro.ORG\ChordPro\chordpro.exe)
-$previewerPath = 'C:\Program Files\Google\Chrome\Application\chrome.exe'
+$previewerPath = Resolve-Path -Path (Join-Path -Path $env:ProgramFiles -ChildPath Google\Chrome\Application\chrome.exe)
+$configPath = Resolve-Path -Path (Join-Path -Path $PSScriptRoot -ChildPath chordpro.json)
 
 foreach ($arg in $Arguments) {
     $inputPath = Resolve-Path -Path $arg
     $item = Get-Item -Path $inputPath
     $outputPath = Join-Path -Path $item.DirectoryName -ChildPath "$($item.BaseName).pdf"
 
-    & $chordProPath $inputPath --output $outputPath
+    & $chordProPath $inputPath --config $configPath --output $outputPath
     $exitCode = $LASTEXITCODE
     if ($exitCode -ne 0) {
         throw "Failed to render $inputPath"
