bf962dff1e7cf31bb0483141a7edc42db0cb97ac
Richard Cook <rcook@rcook.org>
2023-12-30 23:12:46 -0800

    Rename config and regenerate default

--- render.ps1
+++ render.ps1
@@ -8,38 +8,49 @@ param(
 Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
 
+$chordProPath = Resolve-Path -Path (Join-Path -Path $env:ProgramFiles -ChildPath ChordPro.ORG\ChordPro\chordpro.exe)
+
 $previewerPath = $null
 
 if (-not $previewerPath) {
-    $temp = (Join-Path -Path $env:ProgramFiles -ChildPath Google\Chrome\Application\chrome.exe)
+    $temp = Join-Path -Path $env:ProgramFiles -ChildPath Google\Chrome\Application\chrome.exe
     if (Test-Path -Path $temp) {
         $previewerPath = Resolve-Path -Path $temp
     }
 }
 
 if (-not $previewerPath) {
-    $temp = (Join-Path -Path ${env:ProgramFiles(x86)} -ChildPath Google\Chrome\Application\chrome.exe)
+    $temp = Join-Path -Path ${env:ProgramFiles(x86)} -ChildPath Google\Chrome\Application\chrome.exe
     if (Test-Path -Path $temp) {
         $previewerPath = Resolve-Path -Path $temp
     }
 }
 
-$chordProPath = Resolve-Path -Path (Join-Path -Path $env:ProgramFiles -ChildPath ChordPro.ORG\ChordPro\chordpro.exe)
+$configPath = $null
 
-$configPath = Resolve-Path -Path (Join-Path -Path $PSScriptRoot -ChildPath chordpro.json)
+if (-not $configPath) {
+    $temp = Join-Path -Path $PSScriptRoot -ChildPath chordpro-big.json
+    if (Test-Path -Path $temp) {
+        $configPath = Resolve-Path -Path $temp
+    }
+}
 
 foreach ($arg in $Arguments) {
     $inputPath = Resolve-Path -Path $arg
     $item = Get-Item -Path $inputPath
     $outputPath = Join-Path -Path $item.DirectoryName -ChildPath "$($item.BaseName).pdf"
 
-    & $chordProPath $inputPath --config $configPath --output $outputPath
+    if (-not $configPath) {
+        & $chordProPath $inputPath --output $outputPath
+    } else {
+        & $chordProPath $inputPath --config $configPath --output $outputPath
+    }
     $exitCode = $LASTEXITCODE
     if ($exitCode -ne 0) {
         throw "Failed to render $inputPath"
     }
 
-    if ($null -eq $previewerPath) {
+    if (-not $previewerPath) {
         & $outputPath
     } else {
         & $previewerPath $outputPath
